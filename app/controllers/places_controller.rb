53fdeee1f53c3c8efa07325c474c9b257d59e8ff
Andre Yeargins <lonier4@gmail.com>
2020-08-28 19:36:03 +0000

    installed simple_form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,5 +1,9 @@
 class PlacesController < ApplicationController
   def index
-    @places = Place.all
+    @places = Place.all.page(params[:page])
+  end
+
+  def new
+    @place = Place.new
   end
 end
